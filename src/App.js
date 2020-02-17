3598cacfed3e1daa58080b6254bc3bfd3ea1f3d8
cecelia23 <351613102@qq.com>
2020-02-17 16:25:18 +0800

    single page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Clock from './components/Clock.js';
+import ShowInput from './components/showInput.js';
+import Button from './components/Button.js';
+import Check from './components/check.js';
+import TextArea from './components/textarea.js';
+import Twins from './components/ref.js';
+import Count from './components/lifecycle.js';
+import Toggle from './components/toggle.js';
+import MyRef from './components/createRef';
+// import LogControl from './components/logControl.js';
+// import NameForm from './components/form1.js';
+// import SelectForm from './components/form2.js';
+import Reservation from './components/form3.js';
+import RefDemo from './components/form4'
+import Temperature from './components/temperature';
+import SignUpDialog from './components/dialog';
 import './App.css';
 
 function App() {
@@ -7,20 +23,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button />
+        <RefDemo />
+        <ShowInput />
+        <Clock />
+        <MyRef />
+        <Count />
+        <div id='lifecycle'></div>
+        <TextArea />
+        <Check/>
+        <Twins/>
+        <Toggle /> 
+        {/* <NameForm /> */}
+        {/* <SelectForm /> */}
+        <Reservation />
+        {/* <LogControl isLogIn={true}/> */}
+        <Temperature scale='c' />
+        <SignUpDialog />
       </header>
     </div>
   );
 }
+
 
 export default App;
